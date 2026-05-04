f85be0fc96541af60081fcd042a3ba83341a08a6
shivam2k15 <shivamkumar2k16k@gmail.com>
2026-05-04 19:58:52 +0530

    refactor: add minimal tsconfig for TypeScript support

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,5 +1,6 @@
 export const add = (nums: string): number => {
   let sum = 0;
-  nums.split(",").forEach(item=>{sum+=Number(item)});
+  nums.split(",").forEach(item => { sum += Number(item) });
   return sum;
 };
+
