6e3a8e104c698bd4591a89bd4e89412cb7da1e38
shivam2k15 <shivamkumar2k16k@gmail.com>
2026-05-04 19:02:38 +0530

    implement: add function to get the sum of comma-separated numbers

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,3 +1,5 @@
-
-export const add = (numbers: string) => {
-}
+export const add = (nums: string): number => {
+  let sum = 0;
+  nums.split(",").forEach(item=>{sum+=Number(item)});
+  return sum;
+};
